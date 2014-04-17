f229fdf95862d9ac3ee01490953a4756b57a9885
João Melo <jrurtiga@hotmail.com>
2014-04-17 17:02:52 -0300

    Teste

--- Smarket/src/br/com/smarket/view/Main.java
+++ Smarket/src/br/com/smarket/view/Main.java
@@ -9,17 +9,17 @@ public class Main {
     public static void main(String[] args) {
     	try{
 
-    	Usuario user = new Usuario();
-    	user.setAdministrador(false);
-    	user.setLogin("Yuri");
-    	user.setSenha("123");
-    	UsuarioController.CadastrarUsuario(user);
+//    	Usuario user = new Usuario();
+//    	user.setAdministrador(false);
+//    	user.setLogin("Yuri");
+//    	user.setSenha("123");
+//    	UsuarioController.CadastrarUsuario(user);
     	
     	Produto produto = new Produto();
     	produto.setNome("Vasilina");
-    	Secao secao = new Secao();
-    	secao.setNome("SecaoDeVasilina");
-    	produto.setSecao(secao);
+    	//Secao secao = new Secao();
+    	//secao.setNome("SecaoDeVasilina");
+    	produto.setSecao(null);
     	
     	ProdutoDAO produtoDAO = new ProdutoDAO();
     	produtoDAO.insertOrUpdate(produto);
